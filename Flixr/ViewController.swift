33ebd6d5d563626ddda053b4bbb2be8dfba02cce
Natali Siam-Pollo <nataliesiam@yahoo.com>
2022-09-11 23:02:49 -0400

    Initial Commit

---
+++ Flixr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flixr
+//
+//  Created by Natali Siam on 9/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
